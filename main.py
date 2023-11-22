af02db427fae2d75cfc13309f1310178fced3106
wkk4 <wkk4@sfu.ca>
2023-11-22 13:51:03 -0800

    empty py file

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import pyspark.sql import SparkSession, functions, types
+import pandas as pd
+import numpy as np
+
+
+
+def main(in_directory, out_directory):
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    in_directory = sys.argv[1]
+    out_directory = sys.argv[2]
+
+    main(in_directory, out_directory)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
