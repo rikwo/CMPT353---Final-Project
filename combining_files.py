4e60bb9d5cb65dbca71862f5028cd350b1f1d9ae
SPennyh <87890006+SPennyh@users.noreply.github.com>
2023-11-22 22:06:32 -0800

    added combining file

---
+++ combining_files.py
@@ -0,0 +1,15 @@
+import os
+import pathlib
+import sys
+import numpy as np
+import pandas as pd
+
+#use read_excel for .xlsx files
+
+
+def main():
+    input_directory = pathlib.Path(sys.argv[1])
+    output_directory = pathlib.Path(sys.argv[2])
+
+
+main()
