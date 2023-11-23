de34a24a448ec9d119cb1015f93b368c2504fb46
SPennyh <87890006+SPennyh@users.noreply.github.com>
2023-11-23 00:28:32 -0800

    uploaded a pipeline and fixed some data

---
+++ analyze_data.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
---
+++ playoff_predict.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import sys
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+
+def main():
+  
+
+if __name__ == '__main__':
+    main()
