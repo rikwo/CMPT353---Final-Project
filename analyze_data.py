e1a47b2a4dd2b6d16f825aa32cdca3c50da8a133
SPennyh <87890006+SPennyh@users.noreply.github.com>
2023-11-28 01:41:05 -0800

    started a little bit of analysis

--- analyze_data.py
+++ analyze_data.py
@@ -6,6 +6,11 @@ from scipy import stats
 
 
 def main():
+    playoffs = pd.read_csv(sys.argv[1])
+    
+    conference_finalist = playoffs[playoffs['W'] > 7]
+    conference_finalist = conference_finalist[['Team', 'Season']]
+
 
 
 if __name__ == '__main__':
