6abb9146304a5a492f92c2d98d0f8aa469cb8084
Spencer Hall <spencedhall10@gmail.com>
2023-11-28 17:42:36 -0800

    fixed results 1819, prepared playoffs

--- analyze_data.py
+++ analyze_data.py
@@ -6,10 +6,15 @@ from scipy import stats
 
 
 def main():
-    playoffs = pd.read_csv(sys.argv[1])
+    playoffs = pd.read_csv("combined-files\combined_playoff.csv")
+    team_stats = pd.read_csv("combined-files\combined_team_stats.csv")
     
-    conference_finalist = playoffs[playoffs['W'] > 7]
+    conference_finalist = playoffs[(playoffs['W'] > 7) & ~(playoffs['Season'] == 20192020)]
     conference_finalist = conference_finalist[['Team', 'Season']]
+    conference_finalist['Season'] = conference_finalist['Season'].astype('string')
+    conference_finalist['Season'] = conference_finalist['Season'].str.slice_replace(0, 4, '').astype('i')
+
+    print(conference_finalist.dtypes)
 
 
 
